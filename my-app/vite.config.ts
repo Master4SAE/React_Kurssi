18a3fa9930e05dc2ed666c6f61db088d4d5f3ffb
Sohaib_School <sohaibe@metropolia.fi>
2024-03-07 12:10:41 +0200

    custom hooks lap

--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/~sohaibe/hooks/',
+  base: '/~sohaibe/custom-hooks/',
 });
 
